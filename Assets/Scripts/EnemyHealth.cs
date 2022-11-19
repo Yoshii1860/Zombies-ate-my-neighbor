4bbbfd5d9eb7996603711a18a88ba78ecfc522be
Yoshii1860 <yoshii1860@gmail.com>
2022-11-18 20:46:24 -0600

    DeathHandler

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -9,6 +9,8 @@ public class EnemyHealth : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        GetComponent<EnemyAI>().OnDamageTaken();
+    
         hitPoints -= damage;
         if (hitPoints <= 0)
         {
